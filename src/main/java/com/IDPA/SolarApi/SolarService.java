2e13a8f88f043f5ad50c141bce3aa5f568a4386e
MaloJaboulet <malo@cojabou.com>
2022-03-15 17:48:28 +0100

    Änderung der Parameter

--- src/main/java/com/IDPA/SolarApi/SolarService.java
+++ src/main/java/com/IDPA/SolarApi/SolarService.java
@@ -4,6 +4,15 @@ import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+/**
+ * @author Malo Jaboulet
+ * @version 1.2
+ * @since 15.03.2022
+ * <p>
+ * Diese Klass rechnet die Leistung von Solarzellen in verschiedenen Situationen aus.
+ * Es können vier Parameter geändert werden: Wetter, Jahreszeit, Winkel der Solarzelle und Anzahl der Solarzellen.
+ * Aus diesen Parametern wird dann die Leistung ausgerechnet.
+ */
 @Path("solar")
 public class SolarService {
 
@@ -11,33 +20,49 @@ public class SolarService {
     @Path("power")
     @Produces(MediaType.APPLICATION_JSON)
     public Response readDeal(
-            @QueryParam("wetter") String wetter,
-            @QueryParam("zeit") String zeit,
-            @QueryParam("winkel") String winkel,
-            @QueryParam("anzahl") String anzahl
+            @QueryParam("wetter") String wetterS,
+            @QueryParam("jahreszeit") String jahreszeitS,
+            @QueryParam("winkel") String winkelS,
+            @QueryParam("anzahl") String anzahlS
     ) {
         int httpStatus = 200;
-        int power = 0;
+        double power = 0;
+        //Ertrag für Frühling, Sommer, Herbst und Winter in kWh/d/m^2
+        final double[] ertragJahreszeit = {4, 5, 2.5, 1.5};
+        final double panelGroesse = 1.7;            //1.7 m^2 grosse Panel
+        // Faktoren des Wetters: Sonnig, Leicht bewölkt, Stark bewölkt, Trüb/Nebel/Gewitter/Schnee
+        final double[] wetterFaktor = {1, 0.6, 0.3, 0.1};
+        //Maximaler Einfallswinkel der Sonnenstrahlen in den Jahreszeiten Frühling, Sommer, Herbst und Winter
+        final double[] jahreszeitWinkel = {43, 66, 42, 19};
 
         try {
-            int wetterZ = Integer.parseInt(wetter);
-            int zeitZ = Integer.parseInt(zeit);
-            int winkelZ = Integer.parseInt(winkel);
-            int anzahlZ = Integer.parseInt(anzahl);
+            int wetter = Integer.parseInt(wetterS);
+            int jahreszeit = Integer.parseInt(jahreszeitS);
+            int winkel = Integer.parseInt(winkelS);
+            int anzahl = Integer.parseInt(anzahlS);
+
+            if ((wetter <= 4 && wetter >= 0) && (jahreszeit <= 4 && jahreszeit >= 0) &&
+                    (winkel <= 90 && winkel >= 0) && (anzahl <= 100 && anzahl >= 0)) {
+
+                if (wetter == 0 && jahreszeit == 0 && winkel == 0 && anzahl == 0) {
+                    wetter = 1;
+                    jahreszeit = 1;
+                    winkel = 20;
+                    anzahl = 10;
+                }
+
+                double groesse = anzahl / panelGroesse;
+
+                double einfallswinkelSonne = Math.toRadians(jahreszeitWinkel[jahreszeit - 1]);
+                double winkelErtrag = Math.sin((180 - einfallswinkelSonne - winkel));
 
 
-
-            if (wetterZ == 0 && zeitZ == 0 && winkelZ == 0 && anzahlZ == 0) {
-                wetterZ = 1;
-                zeitZ = 800;
-                winkelZ = 20;
-                anzahlZ = 10;
+                power = ertragJahreszeit[jahreszeit - 1] * wetterFaktor[wetter - 1] * groesse * winkelErtrag;
             }
 
-            power = ((wetterZ * zeitZ) / winkelZ) * anzahlZ;
-
-        }catch (NumberFormatException exception){
+        } catch (NumberFormatException exception) {
             httpStatus = 415;
+            power = -1;
         }
         return Response
                 .status(httpStatus)
